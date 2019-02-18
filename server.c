a590134474a3cd27252bb8c1417393d1dc971c0d
p0larstern <41905327+p0larstern@users.noreply.github.com>
2019-02-18 21:12:17 +0530

    added issuer and customer structs in server.c

---
+++ server.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+typedef struct 
+{
+    int id;
+    int num_of_books;
+    int books;
+} issuer;
+
+typedef struct
+{
+    int id;
+    char name;
+    char phone[11];
+} customer;
+
+void issue()
+{
+    
+    return;
+}
+
+int main()
+{
+    return 0;
+}
