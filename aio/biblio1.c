254555b08997359d2e0249416b2f2dca73525a59
p0larstern <41905327+p0larstern@users.noreply.github.com>
2019-04-22 22:12:18 +0530

    beta version ready

--- aio/biblio1.c
+++ aio/biblio1.c
@@ -37,7 +37,7 @@ typedef struct {
 
 // BOOK WHEN LOANED
 typedef struct {
-    char borrow_id[10];
+    char borrow_id[17];
     char isbn[14];
     char user_id[9];
     date *issuing;
@@ -62,7 +62,7 @@ typedef struct bn book_node;
 
 struct un {
     user *user_data;
-    struct bn *next;
+    struct un *next;
 };
 typedef struct un user_node;
 
@@ -71,6 +71,12 @@ struct an {
     struct an *next;
 };
 typedef struct an author_node;
+
+// Making the pointers for various linked lists global for easy usage
+book_node *books_head;
+author_node *authors_head;
+user_node *users_head;
+book_ledgar *lg;
 
 // return's system date
 date *get_date() {
@@ -84,15 +90,102 @@ date *get_date() {
     return new_date;
 }
 
+date *get_return_date(date *og) {
+    int days_in_months[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    og->day += 7;
+    if(og->day > days_in_months[og->month - 1]) {
+        og->day -= days_in_months[og->month - 1];
+        og->month++;
+    }
+    if(og->month == 13) {
+        og->month = 1;
+        og->year++;
+    }
+    return og;
+}
+
+// functions to work book ledger (book loan queue)
+// create book ledgar by book_loaned.bin
+book_ledgar *create_ledger(int max_space) {
+    book_ledgar *new_lg;
+    new_lg = (book_ledgar *)malloc(sizeof(book_ledgar));
+    new_lg->elements = (book_loaned *)malloc(sizeof(book_loaned)*max_space);
+    new_lg->size = 0;
+    new_lg->capacity = max_space;
+    new_lg->front = -1;
+    new_lg->rear = -1;
+    return new_lg;
+}
+
+// check whether the ledger is full or not
+int is_full() {
+    if((lg->front == lg->rear + 1) || (lg->front == 0 && lg->rear == lg->capacity-1)) {
+        return 1;
+    }
+    return 0;
+}
+
+// check whether the ledger is empty or not
+int is_empty() {
+    if(lg->front == -1){
+        return 1;
+    }
+    return 0;
+}
+
+void enqueue(book_loaned *element) {
+    if(lg->front == -1) {
+        lg->front = 0;
+    }
+    lg->rear = (lg->rear + 1) % lg->capacity;
+    lg->elements[lg->rear] = *element;
+}
+
+book_loaned *dequeue() {
+    book_loaned *element;
+    element = (book_loaned *)malloc(sizeof(book_loaned));
+    *element = lg->elements[lg->front];
+    if (lg->front == lg->rear){
+        lg->front = -1;
+        lg->rear = -1;
+    } /* lg has only one element, so we reset the queue after dequeing it. ? */
+    else {
+        lg->front = (lg->front + 1) % lg->capacity;
+    }
+    return element;
+}
+
+/* fill up the ledger from local file */
+void fill_ledger() {
+    book_loaned temp;
+    file_opened = fopen("book_loaned.bin", "rb");
+    
+    while(fread(&temp, sizeof(book_loaned), 1, file_opened)) {
+        enqueue(&temp);
+    }
+    fclose(file_opened);
+}
+
+// updates book_loaned.bin a/c to final ledger
+void finalize_ledger() {
+    file_opened = fopen("book_loaned.bin", "wb");
+    book_loaned *temp;
+
+    while(!is_empty(lg)) {
+        temp = dequeue(lg);
+        fwrite(&temp, sizeof(book_loaned), 1, file_opened);
+    }
+}
+
 book_node *init_book_list() {
-    book temp;
+    book *temp;
     book_node *head, *cur, *temp2;
     file_opened = fopen("book.bin", "rb");
-    
+    temp = (book *)malloc(sizeof(book));
     cur = head;
     while(fread(&temp, sizeof(book), 1, file_opened)) {
         temp2 = (book_node *)malloc(sizeof(book_node));
-        temp2->book_data = &temp;
+        temp2->book_data = temp;
         if(head == NULL) {
             head = temp2;
         } else {
@@ -142,45 +235,115 @@ author_node *init_author_list() {
 }
 
 // BOOKS LINKED LIST EFFECTING FUNCTIONS
-/* generates book id */
-char gen_bid(book *details) {
-    
+
+// This function counts number of leap years before the 
+// given date 
+int count_leap_years(date d) { 
+    int years = d.year; 
+  
+    // Check if the current year needs to be considered 
+    // for the count of leap years or not 
+    if(d.month <= 2) 
+        years--; 
+  
+    // An year is a leap year if it is a multiple of 4, 
+    // multiple of 400 and not a multiple of 100. 
+    return years / 4 - years / 100 + years / 400; 
+} 
+  
+// This function returns number of days between two given 
+// dates 
+int get_difference(date dt1, date dt2) { 
+    int i;
+    int days_in_months[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    // COUNT TOTAL NUMBER OF DAYS BEFORE FIRST DATE 'dt1' 
+  
+    // initialize count using years and day 
+    long int n1 = dt1.year*365 + dt1.day; 
+  
+    // Add days for months in given date 
+    for (i=0; i<dt1.month - 1; i++) 
+        n1 += days_in_months[i]; 
+  
+    // Since every leap year is of 366 days, 
+    // Add a day for every leap year 
+    n1 += count_leap_years(dt1); 
+  
+    // SIMILARLY, COUNT TOTAL NUMBER OF DAYS BEFORE 'dt2' 
+  
+    long int n2 = dt2.year*365 + dt2.day; 
+    for (int i=0; i<dt2.month - 1; i++) 
+        n2 += days_in_months[i]; 
+    n2 += count_leap_years(dt2); 
+  
+    // return difference between two counts 
+    return (n2 - n1); 
+} 
+
+int get_fine(date *final, date *initial) {
+    int fine = 0;
+    fine = get_difference(*final, *initial)*10;
+    return fine;
 }
 
-void add_books() {
-    book new_book;
-    printf("\nISBN?");
-    scanf("%s", new_book.isbn);
-    printf("\nQuantity?");
-    scanf("%d", &new_book.quantity);
-    if(!search_book(&new_book.isbn, 1)) {
-        printf("\nBook Title?");
-        scanf("%s", new_book.title);
-        new_book.deleted = 0;
-    } else {
-            increase_count(&new_book.isbn, new_book.quantity, start);
+void increase_count(char *temp_isbn, int qnty) {
+    book_node *cur;
+    cur = books_head;
+    while(cur != NULL) {
+        if(cur->book_data->isbn == temp_isbn) {
+            cur->book_data->quantity += qnty;
+            return;
         }
+        cur = cur->next;
     }
 }
 
-void delete_books(book_node *start) {
-    int n, i, qnty;
-    char temp_isbn[14];
-    printf("\nNumber of different books to delete?");
-    scanf("%d", &n);
-
-    for(i=0; i<n; i++) {
-        printf("\nBook #%d", i+1);
-        printf("\nISBN?");
-        scanf("%s", temp_isbn);
-        if(search_book(temp_isbn, 1, start)) {
-            printf("\nHow many?");
-            scanf("%d", &qnty);
-            delete_book(temp_isbn, start);
-        } else {
-            printf("\nNo such book found!");
+void decrease_count(char *temp_isbn, int qnty) {
+    book_node *cur;
+    cur = books_head;
+    while(cur != NULL) {
+        if(cur->book_data->isbn == temp_isbn) {
+            cur->book_data->quantity -= qnty;
+            return;
         }
+        cur = cur->next;
     }
+}
+/* return 1 if the requested attribute exists in book linked list else 0
+    1 : isbn
+    2 : title
+    3 : author_id
+*/
+int search_book(char *req_attr, int type_attr) {
+    book_node *cur;
+    cur = books_head;
+    while(cur != NULL) {
+        switch(type_attr) {
+            case 1: if(strcmp(cur->book_data->isbn, req_attr) == 0)
+                        return 1;
+            case 2: if(strcmp(cur->book_data->title, req_attr) == 0)
+                        return 1;
+            case 3: if(strcmp(cur->book_data->author_id, req_attr) == 0)
+                        return 1;
+        }
+        cur = cur->next;
+    }
+    return 0;
+}
+
+/* follow up function for search in book menu */
+void fsearch_book() {
+    system("cls");
+    int search_type;
+    char temp[20];
+    printf("\nSearch by:-\n1.ISBN\n2.Title\n?");
+    scanf("%d", &search_type);
+    printf("\nValue?");
+    scanf("%s", temp);
+    if(search_book(temp, search_type))
+        printf("Book found.");
+    else
+        printf("Book not found");
 }
 
 // add book data to last node of books linked list.
@@ -197,12 +360,28 @@ void add_book(book *book_to_add) {
     printf("\nBook added!");
 }
 
+/* follow up funtion of add book in book menu */
+void fadd_book() {
+    system("cls");
+    book new_book;
+    printf("\nISBN?");
+    scanf("%s", new_book.isbn);
+    printf("\nQuantity?");
+    scanf("%d", &new_book.quantity);
+    if(!search_book(new_book.isbn, 1)) {
+        printf("\nBook Title?");
+        scanf("%s", new_book.title);
+    } else {
+            increase_count(new_book.isbn, new_book.quantity);
+    }
+}
+
 // deletes a book from book.bin
 void delete_book(char *req_isbn) {
     book_node *cur, *cur2;
     cur = books_head;
     if(cur->book_data->isbn == req_isbn) {
-        start = cur->next;
+        books_head = cur->next;
         free(cur);
         return;
     } else {
@@ -222,28 +401,109 @@ void delete_book(char *req_isbn) {
     }
 }
 
-/* return 1 if the requested attribute exists in book.bin else 0
-    1 : isbn
-    2 : title
-    3 : author_id
-*/
-int search_book(char req_attr[], int type_attr) {
-    book_node *cur;
-    cur = books_head;
-    while(cur != NULL) {
-        switch(type_attr) {
-            case 1: if(cur->book_data->isbn == req_attr)
-                        return 1;
-            case 2: if(cur->book_data->title == req_attr)
-                        return 1;
-            case 3: if(cur->book_data->author_id == req_attr)
-                        return 1;
-        }
-        cur = cur->next;
+void fdelete_book() {
+    system("cls");
+    char temp_isbn[14];
+    int qnty;
+    printf("\nISBN?");
+    scanf("%s", temp_isbn);
+    if(search_book(temp_isbn, 1)) {
+        printf("\nHow many?");
+        scanf("%d", &qnty);
+        delete_book(temp_isbn);
+    } else {
+        printf("\nNo such book found!");
     }
-    return 0;
 }
 
+/* generates borrowing id
+example :-
+indices :       0 1 2 3 4 5 6 7 8 9 1 1 2 3 4 5 6 
+isbn :          9 9 9 9 9 9 5 5 5 3 3 2 2 \0
+user_id :       d h r u x x x x \0
+issuing :       23 2 2019
+bor_id :        2 0 1 9 0 2 2 3 x x x x 3 3 2 2 \0
+*/
+char *gen_bor_id(book_loaned *temp) {
+    char *id, a[10], b[5], c[5];
+    id = (char *)malloc(sizeof(char)*17);
+    int i;
+    sprintf(a, "%d", temp->issuing->year);
+    if(temp->issuing->month < 10)
+        sprintf(b, "0%d", temp->issuing->month);
+    else
+        sprintf(b, "%d", temp->issuing->month);
+    if(temp->issuing->day < 10)
+        sprintf(c, "0%d", temp->issuing->day);
+    else
+        sprintf(c, "%d", temp->issuing->day);
+    for(i = 0; i < 4; i++)
+        id[i] = a[i];
+    for(i = 4; i < 6; i++)
+        id[i] = b[i-4];
+    for(i = 6; i < 8; i++)
+        id[i] = c[i-6];
+    for(i = 8; i < 12; i++)
+        id[i] = temp->user_id[i - 8 + 4];
+    for(i = 12; i < 16; i++)
+        id[i] = temp->isbn[i - 12 + 9];
+    id[16] = "\0";
+    return id;
+}
+
+/* search for record in book ledger */
+book_loaned *get_record(char *record_bid) {
+    book_loaned *temp;
+    temp = (book_loaned *)malloc(sizeof(book_loaned));
+    int count = 0;
+    do {
+        temp = dequeue();
+        if(temp->borrow_id == record_bid)
+            return temp;
+        enqueue(temp);
+        count++;
+    } while(count < lg->size);
+    return NULL;
+}
+
+/* issue single book */
+void issue_book() {
+    system("cls");
+    book_loaned *new_issue;
+    new_issue = (book_loaned *)malloc(sizeof(book_loaned));
+    printf("\nUser ID?");
+    scanf("%s", new_issue->user_id);
+    printf("\nISBN?");
+    scanf("%s", new_issue->isbn);
+    new_issue->issuing = get_date();
+    new_issue->returning = get_date();
+    new_issue->returning = get_return_date(new_issue->returning);
+    strcpy(new_issue->borrow_id,gen_bor_id(new_issue));
+    decrease_count(new_issue->isbn, 1);
+    enqueue(new_issue);
+}
+
+/* return book */
+void return_book() {
+    system("cls");
+    book_loaned *record;
+    char temp_id[17];
+    int fine;
+    printf("\nBorrow ID?");
+    scanf("%s", temp_id);
+    record = get_record(temp_id);
+    if(record == NULL) {
+        printf("\nNo such record found.");
+        return;
+    } else {
+        record->returned = get_date();
+        fine = get_fine(record->returned, record->returning);
+        if(fine > 0) {
+            printf("\nPlease pay %d ruppees fine.", fine);
+        }
+        printf("\nThank You.");
+    }
+}
 
 // AUTHORS LINKED LIST EFFECTING FUNCTIONS
 /* generates author_id 
@@ -252,8 +512,9 @@ example :-                0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
     author->last_name -   f r o s t \0
 then author->id -         r o b e f r o s \0
 */
-char gen_aid(author *data) {
-    char id[9];
+char *gen_aid(author *data) {
+    char *id;
+    id = (char *)malloc(sizeof(char)*9);
     int i;
     for(i=0; i<4; i++)
         id[i] = data->first_name[i];
@@ -286,11 +547,11 @@ int search_author(char *req_attr, int type_attr) {
     cur = authors_head;
     while(cur != NULL) {
         switch(type_attr) {
-            case 1: if(cur->author_data->author_id == req_attr)
+            case 1: if(strcmp(cur->author_data->author_id, req_attr) == 0)
                         return 1;
-            case 2: if(cur->author_data->first_name == req_attr)
+            case 2: if(strcmp(cur->author_data->first_name, req_attr) == 0)
                         return 1;
-            case 3: if(cur->author_data->last_name == req_attr)
+            case 3: if(strcmp(cur->author_data->last_name, req_attr) == 0)
                         return 1;
         }
         cur = cur->next;
@@ -300,6 +561,7 @@ int search_author(char *req_attr, int type_attr) {
 
 /* follow up function for menu option of author search */
 void fsearch_author() {
+    system("cls");
     char first_name[15], last_name[15];
     printf("\nFirst name?");
     scanf("%s", first_name);
@@ -313,6 +575,7 @@ void fsearch_author() {
 
 /* printing all the book titles related to requested author */
 void author_books() {
+    system("cls");
     book_node *cur;
     char temp_aid[9];
     printf("\nAuthor ID?");
@@ -340,8 +603,9 @@ example :-
     user->phone -   x x x x x x y y y y \0
 then user->id -     d h r u y y y y \0 
 */
-char gen_uid(user *stuff) {
-    char id[9];
+char *gen_uid(user *stuff) {
+    char *id;
+    id = (char *)malloc(sizeof(char)*9);
     int i;
     for(i=0; i<4; i++)
         id[i] = stuff->name[i];
@@ -366,6 +630,7 @@ int search_user(char *req_user_id) {
 
 /* follow up function of option search user in menu user */ 
 void fsearch_user() {
+    system("cls");
     char temp_uid[11];
     printf("\nUser Id?");
     scanf("%s", temp_uid);
@@ -388,6 +653,7 @@ void add_user(user_node *user_to_add) {
 
 /* follow up function of option add user in menu user */
 void fadd_user() {
+    system("cls");
     user *temp_user_data;
     user_node *new_user;
     temp_user_data = (user *)malloc(sizeof(user));
@@ -433,6 +699,7 @@ void delete_user(char *req_uid) {
 
 /* follow up function of option delete user in menu user */
 void fdelete_user() {
+    system("cls");
     char temp_uid[9];
     printf("\nUser ID?");
     scanf("%s", temp_uid);
@@ -456,86 +723,39 @@ void final_users() {
 }
 /*--------------------------------------------------user functions complete--------------------------------------------*/
 
-// functions to work book ledger (book loan queue)
-// create book ledgar by book_loaned.bin
-book_ledgar *create_ledger(int max_space) {
-    file_opened = fopen("book_loaned.bin", "rb");
-    book_loaned temp;
-    book_ledgar *new_lg;
-
-    new_lg = (book_ledgar *)malloc(sizeof(book_ledgar));
-    new_lg->elements = (book_loaned *)malloc(sizeof(book_loaned)*max_space);
-    new_lg->size = 0;
-    new_lg->capacity = max_space;
-    new_lg->front = -1;
-    new_lg->rear = -1;
-    
-    while(fread(&temp, sizeof(book_loaned), 1, file_opened)) {
-        enqueue(new_lg, &temp);
+void final_books() {
+    file_opened = fopen("book.bin", "wb");
+    book_node *cur;
+    cur = books_head;
+    while(cur != NULL) {
+        fwrite(cur, sizeof(book_node), 1, file_opened);
+        cur = cur->next;
     }
     fclose(file_opened);
-    return new_lg;
 }
 
-// check whether the ledger is full or not
-int is_full(book_ledgar *lg) {
-    if((lg->front == lg->rear + 1) || (lg->front == 0 && lg->rear == lg->capacity-1)) {
-        return 1;
+void final_authors() {
+    file_opened = fopen("author.bin", "wb");
+    author_node *cur;
+    cur = authors_head;
+    while(cur != NULL) {
+        fwrite(cur, sizeof(author_node), 1, file_opened);
+        cur = cur->next;
     }
-    return 0;
-}
-
-// check whether the ledger is empty or not
-int is_empty(book_ledgar *lg) {
-    if(lg->front == -1){
-        return 1;
-    }
-    return 0;
-}
-
-void enqueue(book_ledgar *lg, book_loaned *element) {
-    if(lg->front == -1) {
-        lg->front = 0;
-    }
-    lg->rear = (lg->rear + 1) % lg->capacity;
-    lg->elements[lg->rear] = *element;
-}
-
-book_loaned *dequeue(book_ledgar *lg) {
-    book_loaned *element;
-    element = (book_loaned *)malloc(sizeof(book_loaned));
-    *element = lg->elements[lg->front];
-    if (lg->front == lg->rear){
-        lg->front = -1;
-        lg->rear = -1;
-    } /* lg has only one element, so we reset the queue after dequeing it. ? */
-    else {
-        lg->front = (lg->front + 1) % lg->capacity;
-    }
-    return element;
-}
-
-// updates book_loaned.bin a/c to final ledger
-void finalize_ledger(book_ledgar *lg) {
-    file_opened = fopen("book_loaned.bin", "wb");
-    book_loaned *temp;
-
-    while(!is_empty(lg)) {
-        temp = dequeue(lg);
-        fwrite(&temp, sizeof(book_loaned), 1, file_opened);
-    }
+    fclose(file_opened);
 }
 
 // SUB MENU PRINTING FUNCTIONS
 /* Sub Menu for Books */
 void menu_books() {
+    system("cls");
     int opt;
     printf("\n==================== BOOKS ====================");
     printf("\n1. Add Book");
     printf("\n2. Delete Book");
     printf("\n3. Search Book");
-    printf("\n4. Issue Package");
-    printf("\n5. Return Package");
+    printf("\n4. Issue Book");
+    printf("\n5. Return Book");
     printf("\n6. Go Back");
     printf("\n===============================================");
     printf("\nEnter option:");
@@ -546,8 +766,8 @@ void menu_books() {
             case 1: fadd_book();break;
             case 2: fdelete_book();break;
             case 3: fsearch_book();break;
-            case 4: issue_package();break;
-            case 5: return_package();break;
+            case 4: issue_book();break;
+            case 5: return_book();break;
             case 6: return;
         }
     } while(opt > 6 || opt < 1);
@@ -555,6 +775,7 @@ void menu_books() {
 
 /* Sub Menu For Authors */
 void menu_authors() {
+    system("cls");
     int opt;
     printf("\n=================== AUTHORS ===================");
     printf("\n1. Search Author");
@@ -574,6 +795,7 @@ void menu_authors() {
 
 /* Sub Menu for Users */
 void menu_users() {
+    system("cls");
     int opt;
     printf("\n==================== USERS ====================");
     printf("\n1. Search User");
@@ -594,12 +816,6 @@ void menu_users() {
     } while(opt > 4 || opt < 1);
 }
 
-// Making the pointers for various linked lists global for easy usage
-book_ledgar *ledger;
-book_node *books_head;
-author_node *authors_head;
-user_node *users_head;
-
 void main() {
     int menu_opt;
 
@@ -607,9 +823,11 @@ void main() {
     books_head = init_book_list();
     authors_head = init_author_list();
     users_head = init_user_list();
-    ledger = create_ledger(50);
+    lg = create_ledger(50);
+    fill_ledger();
 
     do {
+        system("cls");
         printf("\n====================MAIN MENU===================");
         printf("\n1. Books");
         printf("\n2. Authors");
@@ -629,6 +847,9 @@ void main() {
         } while(menu_opt > 4 || menu_opt < 1);    
     } while(menu_opt != 4);
 
-    finalize_ledger(ledger);
+    finalize_ledger();
+    final_books();
+    final_users();
+    final_authors();
     exit(0);
 }
